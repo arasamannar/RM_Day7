2299ae9a9dea83716e7d487927114c37e1ba00cd
Arasamannar A K <arasamannar@gmail.com>
2023-09-29 12:35:14 +0530

    Add files via upload
    Day 7 Task files has been added successfully

---
+++ restFunction.js
@@ -0,0 +1,24 @@
+var request = new XMLHttpRequest();
+request.open("GET", "https://restcountries.com/v3.1/all");
+request.send();
+request.onload = function ()
+{
+    var result = JSON.parse(request.response);
+    console.log(result);
+    //filter Asia region of the given REST API Countries.
+    var a = result.filter((x) => x.region == "Asia");
+    console.log(a);
+    //filter population of 2 lakhs
+    var pop = result.filter((x) => x.population < 200000);
+    console.log(pop);
+    //using forEach loop to print the name,capital and flag
+    result.forEach(element => {
+        var name= element.name.common;
+        var capital= element.capital;
+        var flag1= element.flags;
+        console.log(name, capital, flag1);
+    });
+    //print total population using reduce function
+    var p = result.reduce((s,c) => s + parseInt(c.population));
+    console.log(p);
+}
