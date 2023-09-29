442c3f18530fee30bba6b76b0b36438e2d619f88
Arasamannar A K <arasamannar@gmail.com>
2023-09-29 16:16:15 +0530

    Updated changes by adding code for the USD countries

--- restFunction.js
+++ restFunction.js
@@ -5,12 +5,15 @@ request.onload = function ()
 {
     var result = JSON.parse(request.response);
     console.log(result);
+
     //filter Asia region of the given REST API Countries.
     var a = result.filter((x) => x.region == "Asia");
     console.log(a);
+
     //filter population of 2 lakhs
     var pop = result.filter((x) => x.population < 200000);
     console.log(pop);
+
     //using forEach loop to print the name,capital and flag
     result.forEach(element => {
         var name= element.name.common;
@@ -18,7 +21,20 @@ request.onload = function ()
         var flag1= element.flags;
         console.log(name, capital, flag1);
     });
+
     //print total population using reduce function
     var p = result.reduce((s,c) => s + parseInt(c.population));
     console.log(p);
+
+   //print countries which use dollar as currency.
+var cou = result.filter((x) => 
+{
+    if (x.currencies) {
+        let currencies = Object.values(x.currencies);
+        return currencies.some(currency => currency.name && currency.name === 'United States dollar');
+    } else {
+        return false;
+    }
+});
+console.log(cou.map(country => country.name.common));
 }
